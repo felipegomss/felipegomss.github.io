7e2a129e4703ed3f222fd7624ca2c467fdc166eb
Felipe Gomes <51219822+felipegomss@users.noreply.github.com>
2019-11-05 15:00:01 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 $('.slider-principal').slick({
-  variableWidth: true,
   dots: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
+  arrows: false,
+  centerMode: true,
   autoplay: true,
   autoplaySpeed: 2000,
-  arrows: false,
+  infinite: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
   });
